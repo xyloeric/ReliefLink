dcb80eedb12c5ca7c68a57d74fbc01bc0f0f3dc9
Eric Li <xyloeric@gmail.com>
2013-09-17 10:21:28 -0700

    A bunch of project house keeping

--- Anchor/ZLTableSectionHeaderView.h
+++ Anchor/ZLTableSectionHeaderView.h
@@ -13,10 +13,10 @@
 @interface ZLTableSectionHeaderView : UIView
 @property (nonatomic, retain) UILabel *label;
 
-- (id)initWithFrame:(CGRect)frame backgroundColor:(UIColor *)backgroundColor shadowColor:(UIColor *)shadowColor textColor:(UIColor *)textColor textAlignment:(UITextAlignment)textAlignment;
-- (id)initWithFrame:(CGRect)frame gradient:(NSArray *)gradient shadowColor:(UIColor *)shadowColor textColor:(UIColor *)textColor textAlignment:(UITextAlignment)textAlignment;
-- (id)initDarkStyleWithFrame:(CGRect)frame textAlignment:(UITextAlignment)textAlignment;
-- (id)initLightStyleWithFrame:(CGRect)frame textAlignment:(UITextAlignment)textAlignment;
+- (id)initWithFrame:(CGRect)frame backgroundColor:(UIColor *)backgroundColor shadowColor:(UIColor *)shadowColor textColor:(UIColor *)textColor textAlignment:(NSTextAlignment)textAlignment;
+- (id)initWithFrame:(CGRect)frame gradient:(NSArray *)gradient shadowColor:(UIColor *)shadowColor textColor:(UIColor *)textColor textAlignment:(NSTextAlignment)textAlignment;
+- (id)initDarkStyleWithFrame:(CGRect)frame textAlignment:(NSTextAlignment)textAlignment;
+- (id)initLightStyleWithFrame:(CGRect)frame textAlignment:(NSTextAlignment)textAlignment;
 
 - (void)setText:(NSString *)text;
 
